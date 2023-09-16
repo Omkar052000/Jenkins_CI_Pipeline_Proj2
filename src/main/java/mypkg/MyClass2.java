3e7c7522195476b75995616fd8ea4f76ad2bc37a
Omkar Nitin Pathak <omiipathak2000@gmail.com>
2023-09-16 12:23:24 +0530

    added new print statement

--- src/main/java/mypkg/MyClass2.java
+++ src/main/java/mypkg/MyClass2.java
@@ -4,6 +4,7 @@ public class MyClass2 {
 
 	public static void main(String[] args) {
 		System.out.println("Hey, geeks! Welcome to the world of DevOps");
+		System.out.println("Happy Learning!");
 	}
 
 }
